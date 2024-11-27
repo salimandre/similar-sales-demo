7368561ed2042ace39fb0e8ee6b777ecc4ddb4ff
gautier LOVEIKO <55459098+salimandre@users.noreply.github.com>
2024-11-27 13:29:16 +0000

    updating page icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Similar Products", page_icon="🎬")
+st.set_page_config(page_title="Similar Products", page_icon="🏝️")
 st.title("🏝️ Similar Products")
 st.write(
     """
