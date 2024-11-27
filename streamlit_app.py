7c08dc568f3f8b1c14bcd2c9f7a24cbe857ae334
gautier LOVEIKO <55459098+salimandre@users.noreply.github.com>
2024-11-27 13:28:24 +0000

    updating title text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Similar Products", page_icon="🎬")
+st.title("🏝️ Similar Products")
 st.write(
     """
     This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
